c05284f80439039584ef58b0ca124b14908cdf65
xzcabbage <xzcabbage@gmail.com>
2025-07-22 22:14:13 +0800

    Initial commit

---
+++ src/main/java/org/cabbage/algorithm/Main.java
@@ -0,0 +1,11 @@
+package org.cabbage.algorithm;
+
+/**
+ * @author xzcabbage
+ * @since 2025/7/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
